0c1887ea0f3281616b52940dc354e9706bf1942e
SARKARABIR <96632183+SARKARABIR@users.noreply.github.com>
2026-03-06 01:43:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def init_db():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-
+# ---------------------------------  DATABASE CREATION  ---------------------------------#
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS transactions(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,7 +16,7 @@ def init_db():
     description TEXT
     )
     """)
-    
+# ---------------------------------  DATABASE CREATION  ---------------------------------#   
     conn.commit()
     conn.close()
 
@@ -24,8 +24,39 @@ init_db()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+# ---------------------------------  INCOME, EXPENSE, BALANCE LOGIC  ---------------------------------# 
+    cursor.execute("SELECT * FROM transactions")
+    transactions = cursor.fetchall()
 
+    total_income = 0
+    total_expense = 0
+
+    for t in transactions:
+        amount = t[1]
+        type = t[2]
+
+        if type == "income":
+            total_income += amount
+        else:
+            total_expense += amount
+        
+    balance = total_income + total_expense
+    conn.close()
+
+    return render_template(
+        "index.html", 
+        transactions=transactions,
+        total_income = total_income,
+        total_expense = total_expense,
+        balance = balance
+        )
+# ---------------------------------  INCOME, EXPENSE, BALANCE LOGIC  ---------------------------------# 
+
+
+# --------------------------------- INPUT DATA IN TABLE  ---------------------------------# 
 
 @app.route("/add", methods=["GET","POST"])
 def add_transaction():
@@ -39,16 +70,20 @@ def add_transaction():
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute(""""
-        INSERT INTO transactions (amount,type,category,date,description)
-        VALUES(?,?,?,?,?)
-        """,(amount,type,category,date,description))
+        query = """
+        INSERT INTO transactions (amount, type, category, date, description)
+        VALUES (?, ?, ?, ?, ?)
+        """
+        cursor.execute(query, (amount, type, category, date, description))
 
         conn.commit()
         conn.close()
 
         return redirect("/")
     return render_template("add_transaction.html")
+
+# --------------------------------- INPUT DATA IN TABLE  ---------------------------------# 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
